dcc216497c86a82373388c2a2f04c01f6f94bea1
zhang shijie <tmyk104117@126.com>
2017-05-12 18:29:59 +0800

    sdfasdfsasdv pdf

--- MyYoutubeExtractor.py
+++ MyYoutubeExtractor.py
@@ -747,8 +747,7 @@ class MyYoutubeExtractor(InfoExtractor):
             'only_matching': True,
         },
     ]
-    proxies = {'http': 'http://127.0.0.1:8118',
-                   'https': 'https://127.0.0.1:8118'}
+
 
     def __init__(self):
         pass
@@ -775,7 +774,7 @@ class MyYoutubeExtractor(InfoExtractor):
         # Configuration
         ua = UserAgent()
         header = {'User-Agent':str(ua.ie)}
-        webcontent = requests.get(url,header,verify=True,proxies=self.proxies)
+        webcontent = requests.get(url,header,verify=True)
         webcontent.raise_for_status()
         return webcontent.text ;
     def _html_search_meta(self, name, html, display_name=None, fatal=False, **kwargs):
@@ -1472,8 +1471,8 @@ class MyYoutubeExtractor(InfoExtractor):
         for d  in dic['formats']:
             if not (int(d['format_id']) >78):
                 url = d['url'];
-                response = requests.head(url,proxies = self.proxies)
-                d['filesize'] = response.headers['Content-Length']
+                response = requests.head(url)
+                d['filesize']  =  response.headers['Content-Length']
                 wformats.append(d)
 
         dic['formats'] = wformats;
